cb8290ec681e89f196a370d2874c46f05361f62b
hajizadehmasoud5-ui <hajizadehmasoud5@gmail.com>
2026-08-20 02:03:04 +0330

    Remove legacy duplicate chat blueprint from Cloudiva startup

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-from myapp import app, DATA_ROOT
-from alanoffer_chat import create_alanoffer_chat_blueprint
-
-app.register_blueprint(create_alanoffer_chat_blueprint(DATA_ROOT))
+import os
+from myapp import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', '3000')))
